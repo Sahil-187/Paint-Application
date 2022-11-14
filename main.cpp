4c15acbbe7188879fb3507ce3bd95f93c653358e
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-11-14 16:20:09 +0530

    Added click recognition

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <graphics.h>
 #include <bits/stdc++.h>
+#include <graphics.h>
 
 using namespace std;
 int max_x;
@@ -8,32 +8,30 @@ int colorbox_xi;
 int colorbox_yi;
 int colorbox_xf;
 int colorbox_yf;
+POINT Cursor;
 
-bool insideColorBox(int x,int y) {
+bool insideColorBox(int x, int y) {
     return (x >= colorbox_xi && y >= colorbox_yi && x <= colorbox_xf && y <= colorbox_yf);
 }
-void SelectedColorBox(int n)
-{
+
+void SelectedColorBox(int n) {
     setfillstyle(SOLID_FILL, n);
     setcolor(n);
     rectangle(20, 20, 80, 80);
     floodfill(21, 21, n);
 }
 
-struct color
-{
+struct color {
     pair<int, int> start;
     pair<int, int> end;
     int n;
-    void insert(int a, int b)
-    {
+    void insert(int a, int b) {
         start.first = a;
         start.second = b;
         end.first = a + 30;
         end.second = b + 30;
     }
-    void fill(int n)
-    {
+    void fill(int n) {
         this->n = n;
         setcolor(n);
         setfillstyle(SOLID_FILL, n);
@@ -43,17 +41,16 @@ struct color
         rectangle(start.first, start.second, end.first, end.second);
     }
 };
+
 vector<color> v(16);
-void drawColorBox()
-{
+void drawColorBox() {
     floodfill(33, 53, BLACK);
     int s1 = 90;
     int s2 = 90;
     int width = 30;
     vector<color> v(16);
     int j = 0;
-    for (int i = 0; i < 8; i++)
-    {
+    for (int i = 0; i < 8; i++) {
         v[i].insert(s1, 20);
         v[i].fill(j++);
         v[i + 8].insert(s2, 50);
@@ -67,31 +64,25 @@ void drawColorBox()
     colorbox_yf = v[7].end.second;
 }
 
-int selectedColor(int x, int y)
-{
-    for (int i = 0; i < 8; i++)
-    {
-        if (x >= v[i].start.first && x <= v[i].end.first)
-        {
-            if (y >= v[i].start.second && y <= v[i].end.second)
-            {
+int selectedColor(int x, int y) {
+    for (int i = 0; i < 8; i++) {
+        if (x >= v[i].start.first && x <= v[i].end.first) {
+            if (y >= v[i].start.second && y <= v[i].end.second) {
+                // cout << i << endl;
                 SelectedColorBox(i);
                 return i;
+            } else {
+                SelectedColorBox(i + 8);
+                return i + 8;
             }
-            else
-                {
-                    SelectedColorBox(i + 8);
-                    return i + 8;
-                }
         }
     }
-    
+
     SelectedColorBox(0);
     return 0;
 }
 
-void initial_screen()
-{
+void initial_screen() {
     int width = 100;
     setcolor(LIGHTGRAY);
     rectangle(0, 0, max_x - 1, width);
@@ -109,23 +100,32 @@ void initial_screen()
     drawColorBox();
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     max_x = GetSystemMetrics(SM_CXSCREEN);
     max_y = GetSystemMetrics(SM_CYSCREEN);
     initwindow(max_x, max_y, "My Paint", -3, 0);
     initial_screen();
-    getch();
-    while(true) {
-        int x,y;
-        
-        x = mousex();
-        y = mousey();
-        cout<<x<<" "<<y<<endl;
-        if(insideColorBox(x,y)) {
-            selectedColor(x,y);
+
+    // for (int i = 0; i < 16; ++i) {
+    //     cout << v[i].start.first << ' ' << v[i].start.second << ' ' << v[i].end.first << ' ' << v[i].end.second << ' ' << v[i].n << '\n';
+    // }
+
+    while (true) {
+        GetCursorPos(&Cursor);
+        ScreenToClient(GetForegroundWindow(), &Cursor);
+        int x, y;
+
+        x = Cursor.x;
+        y = Cursor.y;
+        if (GetAsyncKeyState(VK_LBUTTON)) {
+            if (insideColorBox(x, y)) {
+                cout << x << " " << y << endl;
+                selectedColor(x, y);
+            }
         }
     }
+
     closegraph();
+    getch();
     return 0;
 }
